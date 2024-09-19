3b9114ed762f3af780e4d2455ba95641e8d4f75a
amkhrjee <aniruddha.mukherji@outlook.com>
2024-09-20 02:11:42 +0530

    add: desktop features

--- js/background.js
+++ js/background.js
@@ -1,35 +1,98 @@
 browser.runtime.onMessage.addListener((message, _sender, sendResponse) => {
-  if (message.action === "on-page-load") {
-    /*
-      Checks on page load whether the font for the domain already exists in the storage
-      If it does, then gets the font and applies it to the page
-    */
-    browser.storage.local
-      .get([message.domain])
-      .then((result) => {
-        const fontData = result[message.domain];
-        if (fontData) {
-          console.log("Font Found!");
-          sendResponse({
-            type: "apply_font",
-            data: {
-              serif: fontData.serif,
-              sans_serif: fontData.sans_serif,
-              monospace: fontData.monospace,
-            },
-          });
-        } else {
-          sendResponse({
-            type: "none",
-          });
-          console.log("Font Not Found");
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
-  // This must be returning true to keep listening
-  // read more: https://stackoverflow.com/a/56483156/12404524
-  return true;
+    if (message.action === "on-page-load") {
+        /*
+          Checks on page load whether the font for the domain already exists in the storage
+          If it does, then gets the font and applies it to the page
+        */
+        // check if global apply is turned on
+        browser.storage.sync.get(["global"]).then((is_global) => {
+            browser.storage.sync.get(["override"]).then((is_override) => {
+                browser.storage.sync
+                    .get(["exempts"])
+                    .then((exempts_list) => {
+                    if ("global" in is_global && is_global["global"]) {
+                        if ("exempts" in exempts_list &&
+                            exempts_list["exempts"].includes(message.domain)) {
+                            console.log("GLOBAL MODE IS ON + SITE IS EXEMPTED");
+                            browser.storage.sync.get([message.domain]).then((is_domain) => {
+                                if (message.domain in is_domain) {
+                                    // @ts-ignore
+                                    sendResponse({
+                                        type: "apply_font",
+                                        data: is_domain[message.domain],
+                                    });
+                                }
+                                else {
+                                    // @ts-ignore
+                                    sendResponse({
+                                        type: "none",
+                                    });
+                                }
+                            });
+                        }
+                        else if ("override" in is_override && is_override["override"]) {
+                            // global & override both are on so apply global fonts to all sites
+                            console.log("GLOBAL IS ON + OVERRIDE IS ON");
+                            browser.storage.sync.get(["global_fonts"]).then((result) => {
+                                if ("global_fonts" in result) {
+                                    //@ts-ignore
+                                    sendResponse({
+                                        type: "apply_font",
+                                        data: result["global_fonts"],
+                                    });
+                                }
+                            });
+                        }
+                        else {
+                            console.log("GLOBAL IS ON + OVERRIDE IS OFF");
+                            browser.storage.sync.get([message.domain]).then((is_domain) => {
+                                if (message.domain in is_domain) {
+                                    console.log("GLOBAL IS ON + OVERRIDE IS OFF + Domain has set fonts");
+                                    // @ts-ignore
+                                    sendResponse({
+                                        type: "apply_font",
+                                        data: is_domain[message.domain],
+                                    });
+                                }
+                                else {
+                                    console.log("GLOBAL IS ON + OVERRIDE IS OFF + Domain does not have set fonts");
+                                    browser.storage.sync
+                                        .get(["global_fonts"])
+                                        .then((result) => {
+                                        if ("global_fonts" in result) {
+                                            //@ts-ignore
+                                            sendResponse({
+                                                type: "apply_font",
+                                                data: result["global_fonts"],
+                                            });
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    }
+                    else {
+                        browser.storage.sync.get([message.domain]).then((is_domain) => {
+                            if (message.domain in is_domain) {
+                                // @ts-ignore
+                                sendResponse({
+                                    type: "apply_font",
+                                    data: is_domain[message.domain],
+                                });
+                            }
+                            else {
+                                // @ts-ignore
+                                sendResponse({
+                                    type: "none",
+                                });
+                            }
+                        });
+                    }
+                });
+            });
+        });
+    }
+    // This must be returning true to keep listening
+    // read more: https://stackoverflow.com/a/56483156/12404524
+    return true;
 });
